0011408f4a12f40e43583098ec73e3fb8de590d7
Derek Miranda <derek.miranda@ff0000.com>
2017-12-01 12:11:38 -0800

    Imp/exports

---
+++ index.js
@@ -0,0 +1,66 @@
+
+import Align from './Align'
+import CanvasBlendMode from './canvas/CanvasBlendMode'
+import CanvasBlur from './canvas/CanvasBlur'
+import CanvasCircle from './canvas/CanvasCircle'
+import CanvasColoring from './canvas/CanvasColoring'
+import CanvasDrawer from './canvas/CanvasDrawer'
+import CanvasElement from './canvas/CanvasElement'
+import CanvasImage from './canvas/CanvasImage'
+import CanvasLineTo from './canvas/CanvasLineTo'
+import CanvasPath from './canvas/CanvasPath'
+import CanvasRect from './canvas/CanvasRect'
+import CanvasShape from './canvas/CanvasShape'
+import CanvasSprite from './canvas/CanvasSprite'
+import CanvasText from './canvas/CanvasText'
+import CanvasTexture from './canvas/CanvasTexture'
+import CanvasTiling from './canvas/CanvasTiling'
+import CanvasTweener from './canvas/CanvasTweener'
+import CanvasUtils from './canvas/CanvasUtils'
+import Clamp from './Clamp'
+import Countdown from './Countdown'
+import Effects from './Effects'
+import FullScreen from './FullScreen'
+import Emitter from './particles/Emitter'
+import Graphic from './particles/Graphic'
+import Particle from './particles/Particle'
+import ParticleModel from './particles/ParticleModel'
+import ParticlesUtils from './particles/ParticlesUtils'
+import Ratio from './Ratio'
+
+// named exports
+import * as Styles from './Styles'
+import * as Markup from './Markup'
+
+export {
+	Align,
+	CanvasBlendMode,
+	CanvasBlur,
+	CanvasCircle,
+	CanvasColoring,
+	CanvasDrawer,
+	CanvasElement,
+	CanvasImage,
+	CanvasLineTo,
+	CanvasPath,
+	CanvasRect,
+	CanvasShape,
+	CanvasSprite,
+	CanvasText,
+	CanvasTexture,
+	CanvasTiling,
+	CanvasTweener,
+	CanvasUtils,
+	Clamp,
+	Countdown,
+	Effects,
+	FullScreen,
+	Markup,
+	Emitter,
+	Graphic,
+	Particle,
+	ParticleModel,
+	ParticlesUtils,
+	Ratio,
+	Styles,
+}
