bbcdfcde31e58dda026a496c99dda2f8a1ce71fe
Derek Miranda <derek.miranda@ff0000.com>
2017-12-01 11:11:30 -0800

    Init

---
+++ index.js
@@ -0,0 +1,26 @@
+
+import ColorUtils from './lib/ColorUtils'
+import DcsUtils from './lib/DcsUtils'
+import ImageUtils from './lib/ImageUtils'
+import LocationUtils from './lib/LocationUtils'
+import MonetUtils from './lib/MonetUtils'
+import MotionUtils from './lib/MotionUtils'
+import ObjectUtils from './lib/ObjectUtils'
+import TextUtils from './lib/TextUtils'
+
+// has named exports
+import * as ArrayUtils from './lib/ArrayUtils'
+import * as MathUtils from './lib/MathUtils'
+
+export {
+	ArrayUtils,
+	ColorUtils,
+	DcsUtils,
+	ImageUtils,
+	LocationUtils,
+	MathUtils,
+	MonetUtils,
+	MotionUtils,
+	ObjectUtils,
+	TextUtils,
+}
