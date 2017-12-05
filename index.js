dc775b25261523f32651ef964922679d7ee067b9
Derek Miranda <derek.miranda@ff0000.com>
2017-12-05 09:52:51 -0800

    Temporarily removed Canvas exports, added other imports

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 
 import Align from './lib/Align'
-import CanvasBlendMode from './lib/canvas/CanvasBlendMode'
-import CanvasBlur from './lib/canvas/CanvasBlur'
-import CanvasCircle from './lib/canvas/CanvasCircle'
-import CanvasColoring from './lib/canvas/CanvasColoring'
-import CanvasDrawer from './lib/canvas/CanvasDrawer'
-import CanvasElement from './lib/canvas/CanvasElement'
-import CanvasImage from './lib/canvas/CanvasImage'
-import CanvasLineTo from './lib/canvas/CanvasLineTo'
-import CanvasPath from './lib/canvas/CanvasPath'
-import CanvasRect from './lib/canvas/CanvasRect'
-import CanvasShape from './lib/canvas/CanvasShape'
-import CanvasSprite from './lib/canvas/CanvasSprite'
-import CanvasText from './lib/canvas/CanvasText'
-import CanvasTexture from './lib/canvas/CanvasTexture'
-import CanvasTiling from './lib/canvas/CanvasTiling'
-import CanvasTweener from './lib/canvas/CanvasTweener'
-import CanvasUtils from './lib/canvas/CanvasUtils'
+// import CanvasBlendMode from './lib/canvas/CanvasBlendMode'
+// import CanvasBlur from './lib/canvas/CanvasBlur'
+// import CanvasCircle from './lib/canvas/CanvasCircle'
+// import CanvasColoring from './lib/canvas/CanvasColoring'
+// import CanvasDrawer from './lib/canvas/CanvasDrawer'
+// import CanvasElement from './lib/canvas/CanvasElement'
+// import CanvasImage from './lib/canvas/CanvasImage'
+// import CanvasLineTo from './lib/canvas/CanvasLineTo'
+// import CanvasPath from './lib/canvas/CanvasPath'
+// import CanvasRect from './lib/canvas/CanvasRect'
+// import CanvasShape from './lib/canvas/CanvasShape'
+// import CanvasSprite from './lib/canvas/CanvasSprite'
+// import CanvasText from './lib/canvas/CanvasText'
+// import CanvasTexture from './lib/canvas/CanvasTexture'
+// import CanvasTiling from './lib/canvas/CanvasTiling'
+// import CanvasTweener from './lib/canvas/CanvasTweener'
+// import CanvasUtils from './lib/canvas/CanvasUtils'
 import Clamp from './lib/Clamp'
 import Countdown from './lib/Countdown'
 import Effects from './lib/Effects'
@@ -34,23 +34,23 @@ import * as Markup from './lib/Markup'
 
 export {
 	Align,
-	CanvasBlendMode,
-	CanvasBlur,
-	CanvasCircle,
-	CanvasColoring,
-	CanvasDrawer,
-	CanvasElement,
-	CanvasImage,
-	CanvasLineTo,
-	CanvasPath,
-	CanvasRect,
-	CanvasShape,
-	CanvasSprite,
-	CanvasText,
-	CanvasTexture,
-	CanvasTiling,
-	CanvasTweener,
-	CanvasUtils,
+	// CanvasBlendMode,
+	// CanvasBlur,
+	// CanvasCircle,
+	// CanvasColoring,
+	// CanvasDrawer,
+	// CanvasElement,
+	// CanvasImage,
+	// CanvasLineTo,
+	// CanvasPath,
+	// CanvasRect,
+	// CanvasShape,
+	// CanvasSprite,
+	// CanvasText,
+	// CanvasTexture,
+	// CanvasTiling,
+	// CanvasTweener,
+	// CanvasUtils,
 	Clamp,
 	Countdown,
 	Effects,
