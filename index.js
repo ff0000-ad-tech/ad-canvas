b7403665f6a1bbb565c417a66b5c41a0e488923e
Derek Miranda <derek.miranda@ff0000.com>
2017-12-13 11:34:07 -0800

    Exporting directly from view modules

--- index.js
+++ index.js
@@ -1,66 +1,37 @@
-
-import Align from './lib/Align'
-import CanvasBlendMode from './lib/canvas/CanvasBlendMode'
-import CanvasBlur from './lib/canvas/CanvasBlur'
-import CanvasCircle from './lib/canvas/CanvasCircle'
-import CanvasColoring from './lib/canvas/CanvasColoring'
-import CanvasDrawer from './lib/canvas/CanvasDrawer'
-import CanvasElement from './lib/canvas/CanvasElement'
-import CanvasImage from './lib/canvas/CanvasImage'
-import CanvasLineTo from './lib/canvas/CanvasLineTo'
-import CanvasPath from './lib/canvas/CanvasPath'
-import CanvasRect from './lib/canvas/CanvasRect'
-import CanvasShape from './lib/canvas/CanvasShape'
-import CanvasSprite from './lib/canvas/CanvasSprite'
-import CanvasText from './lib/canvas/CanvasText'
-import CanvasTexture from './lib/canvas/CanvasTexture'
-import CanvasTiling from './lib/canvas/CanvasTiling'
-import CanvasTweener from './lib/canvas/CanvasTweener'
-import CanvasUtils from './lib/canvas/CanvasUtils'
-import Clamp from './lib/Clamp'
-import Countdown from './lib/Countdown'
-import Effects from './lib/Effects'
-import FullScreen from './lib/FullScreen'
-import Emitter from './lib/particles/Emitter'
-import Graphic from './lib/particles/Graphic'
-import Particle from './lib/particles/Particle'
-import ParticleModel from './lib/particles/ParticleModel'
-import ParticlesUtils from './lib/particles/ParticlesUtils'
-import Ratio from './lib/Ratio'
+export { default as Align } from './lib/Align'
+export { default as CanvasBlendMode } from './lib/canvas/CanvasBlendMode'
+export { default as CanvasBlur } from './lib/canvas/CanvasBlur'
+export { default as CanvasCircle } from './lib/canvas/CanvasCircle'
+export { default as CanvasColoring } from './lib/canvas/CanvasColoring'
+export { default as CanvasDrawer } from './lib/canvas/CanvasDrawer'
+export { default as CanvasElement } from './lib/canvas/CanvasElement'
+export { default as CanvasImage } from './lib/canvas/CanvasImage'
+export { default as CanvasLineTo } from './lib/canvas/CanvasLineTo'
+export { default as CanvasPath } from './lib/canvas/CanvasPath'
+export { default as CanvasRect } from './lib/canvas/CanvasRect'
+export { default as CanvasShape } from './lib/canvas/CanvasShape'
+export { default as CanvasSprite } from './lib/canvas/CanvasSprite'
+export { default as CanvasText } from './lib/canvas/CanvasText'
+export { default as CanvasTexture } from './lib/canvas/CanvasTexture'
+export { default as CanvasTiling } from './lib/canvas/CanvasTiling'
+export { default as CanvasTweener } from './lib/canvas/CanvasTweener'
+export { default as CanvasUtils } from './lib/canvas/CanvasUtils'
+export { default as Clamp } from './lib/Clamp'
+export { default as Countdown } from './lib/Countdown'
+export { default as Effects } from './lib/Effects'
+export { default as FullScreen } from './lib/FullScreen'
+export { default as Emitter } from './lib/particles/Emitter'
+export { default as Graphic } from './lib/particles/Graphic'
+export { default as Particle } from './lib/particles/Particle'
+export { default as ParticleModel } from './lib/particles/ParticleModel'
+export { default as ParticlesUtils } from './lib/particles/ParticlesUtils'
+export { default as Ratio } from './lib/Ratio'
 
 // named exports
 import * as Styles from './lib/Styles'
 import * as Markup from './lib/Markup'
 
 export {
-	Align,
-	CanvasBlendMode,
-	CanvasBlur,
-	CanvasCircle,
-	CanvasColoring,
-	CanvasDrawer,
-	CanvasElement,
-	CanvasImage,
-	CanvasLineTo,
-	CanvasPath,
-	CanvasRect,
-	CanvasShape,
-	CanvasSprite,
-	CanvasText,
-	CanvasTexture,
-	CanvasTiling,
-	CanvasTweener,
-	CanvasUtils,
-	Clamp,
-	Countdown,
-	Effects,
-	FullScreen,
 	Markup,
-	Emitter,
-	Graphic,
-	Particle,
-	ParticleModel,
-	ParticlesUtils,
-	Ratio,
 	Styles,
 }
