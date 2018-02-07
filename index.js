d92f627d6d812393b296a05eb5863b46df499d7a
Andrew Dowis <andrew.dowis@andrewd-mbp.ff0000.com>
2018-02-06 16:23:27 -0800

    canvas package migrated

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-export { default as CanvasBlendMode } from './lib/CanvasBlendMode'
-export { default as CanvasBlur } from './lib/CanvasBlur'
-export { default as CanvasCircle } from './lib/CanvasCircle'
-export { default as CanvasColoring } from './lib/CanvasColoring'
 export { default as CanvasDrawer } from './lib/CanvasDrawer'
 export { default as CanvasElement } from './lib/CanvasElement'
-export { default as CanvasImage } from './lib/CanvasImage'
-export { default as CanvasLineTo } from './lib/CanvasLineTo'
-export { default as CanvasPath } from './lib/CanvasPath'
-export { default as CanvasRect } from './lib/CanvasRect'
 export { default as CanvasShape } from './lib/CanvasShape'
-export { default as CanvasSprite } from './lib/CanvasSprite'
+export { default as CanvasPath } from './lib/CanvasPath'
+export { default as CanvasCircle } from './lib/CanvasCircle'
+export { default as CanvasRect } from './lib/CanvasRect'
+export { default as CanvasImage } from './lib/CanvasImage'
 export { default as CanvasText } from './lib/CanvasText'
-export { default as CanvasTexture } from './lib/CanvasTexture'
-export { default as CanvasTiling } from './lib/CanvasTiling'
-export { default as CanvasTweener } from './lib/CanvasTweener'
-
+export { default as CanvasTweenerBase } from './lib/CanvasTweenerBase'
+export { default as CanvasSprite } from './lib/CanvasSprite'
 
 // named exports
-// test
+import * as CanvasBlendMode from './lib/CanvasBlendMode'
+import * as CanvasBlur from './lib/CanvasBlur'
+import * as CanvasColoring from './lib/CanvasColoring'
+import * as CanvasLineTo from './lib/CanvasLineTo'
+import * as CanvasTexture from './lib/CanvasTexture'
+import * as CanvasTiling from './lib/CanvasTiling'
+import * as CanvasTweener from './lib/CanvasTweener'
 import * as CanvasUtils from './lib/CanvasUtils'
 
 export {
-	CanvasUtils,
+	CanvasBlendMode,
+	CanvasBlur,
+	CanvasColoring,
+	CanvasLineTo,
+	CanvasTexture,
+	CanvasTiling,
+	CanvasTweener,
+	CanvasUtils
 }
